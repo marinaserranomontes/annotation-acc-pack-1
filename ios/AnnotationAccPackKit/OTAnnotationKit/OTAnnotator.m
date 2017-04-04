d0885a3149e8b0467bc4b08d7b34a379707b987e
marinas <marinaserranomontes@gmail.com>
2017-04-04 11:12:23 +0200

    Fixing wrong conflict

--- ios/AnnotationAccPackKit/OTAnnotationKit/OTAnnotator.m
+++ ios/AnnotationAccPackKit/OTAnnotationKit/OTAnnotator.m
@@ -452,7 +452,7 @@ receivedSignalType:(NSString*)type
     // iPad aspect ratio is 4:3 = 0.75
     
     CGFloat scale = 1.0f;
-    if (thisCanvasWidth < thisCanvasHeight) {
+    if (thisCanvasWidth < thisCanvasHeight || remoteCanvasWidth < remoteCanvasHeight) {
         scale = thisCanvasHeight / remoteCanvasHeight;
     }
     else {
